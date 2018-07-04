b0d569ad66841d18025427b2aa2132460915e662
JeffreyPing <jeffrey11223@google.com>
2018-07-04 16:54:35 +0800

    Protection against SQL Injection

--- h2db.java
+++ h2db.java
@@ -4,6 +4,7 @@ public class h2db {
         Class.forName("org.h2.Driver");
         Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "root", "0000");
         Statement stmt = conn.createStatement();
+        PreparedStatement ps;
         ResultSet rs;
 
         stmt.execute("CREATE TABLE IF NOT EXISTS DATA(ID INT NOT NULL AUTO_INCREMENT, OWNER VARCHAR(255), ITEM VARCHAR(255), AMOUNT INT)");
@@ -12,31 +13,56 @@ public class h2db {
 
         switch(a[0]){
             case "login":
-                rs = stmt.executeQuery("SELECT * FROM USER WHERE ACCOUNT = '" + a[1] + "'");
+                ps = conn.prepareStatement("SELECT * FROM USER WHERE ACCOUNT = ?");
+                ps.setString(1, a[1]);
+                rs = ps.executeQuery();
+                //rs = stmt.executeQuery("SELECT * FROM USER WHERE ACCOUNT = '" + a[1] + "'");
                 while (rs.next()) {
                   System.out.print(rs.getString("PASSWORD"));
                 }
                 break;
             case "register":
-                rs = stmt.executeQuery("SELECT * FROM USER WHERE ACCOUNT = '" + a[1] + "'");
+                ps = conn.prepareStatement("SELECT * FROM USER WHERE ACCOUNT = ?");
+                ps.setString(1, a[1]);
+                rs = ps.executeQuery();
+                //rs = stmt.executeQuery("SELECT * FROM USER WHERE ACCOUNT = '" + a[1] + "'");
                 if (!rs.next()){
-                    stmt.execute("INSERT INTO USER(ACCOUNT, PASSWORD) VALUES('" + a[1] + "','" + a[2] + "')");
+                    ps = conn.prepareStatement("INSERT INTO USER(ACCOUNT, PASSWORD) VALUES(?,?)");
+                    ps.setString(1, a[1]);
+                    ps.setString(2, a[2]);
+                    ps.execute();
+                    //stmt.execute("INSERT INTO USER(ACCOUNT, PASSWORD) VALUES('" + a[1] + "','" + a[2] + "')");
                 }else System.out.print("used");
                 break;
             case "load":
-                rs = stmt.executeQuery("SELECT * FROM DATA WHERE OWNER = '" + a[1] + "'");
+                ps = conn.prepareStatement("SELECT * FROM DATA WHERE OWNER = ?");
+                ps.setString(1, a[1]);
+                rs = ps.executeQuery();
+                //rs = stmt.executeQuery("SELECT * FROM DATA WHERE OWNER = '" + a[1] + "'");
                 while (rs.next()) {
                   System.out.print(rs.getString("ID") + "," + rs.getString("ITEM") + "," + rs.getString("AMOUNT") + ";");
                 }
                 break;
             case "add":
-                stmt.execute("INSERT INTO DATA(OWNER, ITEM, AMOUNT) VALUES('" + a[1] + "','" + a[2] + "'," + a[3] + ")");
+                ps = conn.prepareStatement("INSERT INTO DATA(OWNER, ITEM, AMOUNT) VALUES(?,?,?)");
+                ps.setString(1, a[1]);
+                ps.setString(2, a[2]);
+                ps.setString(3, a[3]);
+                ps.execute();
+                //stmt.execute("INSERT INTO DATA(OWNER, ITEM, AMOUNT) VALUES('" + a[1] + "','" + a[2] + "'," + a[3] + ")");
                 break;
             case "delete":
-                stmt.execute("DELETE FROM DATA WHERE ID = " + a[1]);
+                ps = conn.prepareStatement("DELETE FROM DATA WHERE ID = ?");
+                ps.setString(1, a[1]);
+                ps.execute();
+                //stmt.execute("DELETE FROM DATA WHERE ID = " + a[1]);
                 break;
             case "update":
-                stmt.execute("UPDATE DATA SET AMOUNT=" + a[1] + " WHERE ID=" + a[2]);
+                ps = conn.prepareStatement("UPDATE DATA SET AMOUNT=? WHERE ID=?");
+                ps.setString(1, a[1]);
+                ps.setString(2, a[2]);
+                ps.execute();
+                //stmt.execute("UPDATE DATA SET AMOUNT=" + a[1] + " WHERE ID=" + a[2]);
                 break;
         }
         conn.close();
